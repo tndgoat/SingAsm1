6ac2c7f045ad97d1236174dabb34e5566aa48bb4
tndgoat <tungnd.goat@gmail.com>
2024-06-21 15:58:36 +0700

    update

--- android/app/src/main/java/com/tndgoat/singasm1/MainActivity.kt
+++ android/app/src/main/java/com/tndgoat/singasm1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.evenhub
+package com.singasm1
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "evenhub"
+  override fun getMainComponentName(): String = "singasm1"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/tndgoat/singasm1/MainApplication.kt
+++ android/app/src/main/java/com/tndgoat/singasm1/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.evenhub
+package com.singasm1
 
 import android.app.Application
 import com.facebook.react.PackageList
