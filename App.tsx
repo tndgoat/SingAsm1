883f5211bcded9bcc3267bd6bbd220fc1fa835af
tndgoat <tungnd.goat@gmail.com>
2024-06-30 17:17:40 +0700

    ok

--- App.tsx
+++ App.tsx
@@ -1,26 +1,118 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {StatusBar} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  StatusBar,
+  PermissionsAndroid,
+  Linking,
+  ActivityIndicator,
+} from 'react-native';
 import {Provider} from 'react-redux';
 import AppRouters from './src/navigators/AppRouters';
 import store from './src/redux/store';
+import messaging from '@react-native-firebase/messaging';
 
-const App = () => {
-  return (
-    <>
-      <Provider store={store}>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor="transparent"
-          translucent
-        />
+const NAVIGATION_IDS = ['onboarding', 'notifi'];
 
-        <NavigationContainer>
-          <AppRouters />
-        </NavigationContainer>
-      </Provider>
-    </>
-  );
+function buildDeepLinkFromNotificationData(data: any): string | null {
+  const navigationId = data?.navigationId;
+  if (!NAVIGATION_IDS.includes(navigationId)) {
+    console.warn('Unverified navigationId', navigationId);
+    return null;
+  }
+  if (navigationId === 'onboarding') {
+    return 'myapp://onboarding';
+  }
+  if (navigationId === 'notifi') {
+    return 'myapp://notifi';
+  }
+
+  return null;
+}
+
+const linking = {
+  prefixes: ['myapp://'],
+  config: {
+    screens: {
+      OnboardingScreen: 'onboarding',
+      NotifiScreen: 'notifi',
+    },
+  },
+  async getInitialURL() {
+    const url = await Linking.getInitialURL();
+    if (typeof url === 'string') {
+      return url;
+    }
+    //getInitialNotification: When the application is opened from a quit state.
+    const message = await messaging().getInitialNotification();
+    const deeplinkURL = buildDeepLinkFromNotificationData(message?.data);
+    if (typeof deeplinkURL === 'string') {
+      return deeplinkURL;
+    }
+  },
+  subscribe(listener: (url: string) => void) {
+    const onReceiveURL = ({url}: {url: string}) => listener(url);
+
+    // Listen to incoming links from deep linking
+    const linkingSubscription = Linking.addEventListener('url', onReceiveURL);
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    const foreground = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', remoteMessage);
+    });
+    //onNotificationOpenedApp: When the application is running, but in the background.
+    const unsubscribe = messaging().onNotificationOpenedApp(remoteMessage => {
+      const url = buildDeepLinkFromNotificationData(remoteMessage.data);
+      if (typeof url === 'string') {
+        listener(url);
+      }
+    });
+
+    return () => {
+      linkingSubscription.remove();
+      unsubscribe();
+      foreground();
+    };
+  },
 };
+
+function App(): React.JSX.Element {
+  useEffect(() => {
+    const requestUserPermission = async () => {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+        const token = await messaging().getToken();
+        console.log('FCM token:', token);
+      }
+    };
+
+    requestUserPermission();
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <NavigationContainer
+        linking={linking}
+        fallback={<ActivityIndicator animating />}>
+        <AppRouters />
+      </NavigationContainer>
+    </Provider>
+  );
+}
 
 export default App;
--- src/screens/notifi/NotifiScreen.tsx
+++ src/screens/notifi/NotifiScreen.tsx
@@ -1,9 +1,10 @@
-import {View, Image} from 'react-native';
+import {View, Image, StatusBar} from 'react-native';
 import React from 'react';
 import {appColors} from './../../constants/appColors';
 import {appInfo} from '../../constants/appInfos';
 import {fontFamilies} from '../../constants/fontFamilies';
 import {ButtonComponent, SpaceComponent, TextComponent} from '../../components';
+import {globalStyles} from '../../styles/globalStyles';
 
 const NotifiScreen = () => {
   return (
@@ -19,6 +20,13 @@ const NotifiScreen = () => {
         color={appColors.text1}
         size={22}
         font={fontFamilies.bold}
+        styles={{textAlign: 'center'}}
+      />
+      <TextComponent
+        text="You're already logged in!"
+        color={appColors.text2}
+        size={16}
+        font={fontFamilies.semiBold}
         styles={{textAlign: 'center'}}
       />
     </View>
